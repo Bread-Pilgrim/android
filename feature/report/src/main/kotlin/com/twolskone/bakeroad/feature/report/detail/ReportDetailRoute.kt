b04f2c159ed2cfcc705b4e530990da9dc5c94efc
sjunh812 <sjunh812@gmail.com>
2025-09-02 18:02:09 +0900

    [fix] report : BaseComposable 누락

--- feature/report/src/main/kotlin/com/twolskone/bakeroad/feature/report/detail/ReportDetailRoute.kt
+++ feature/report/src/main/kotlin/com/twolskone/bakeroad/feature/report/detail/ReportDetailRoute.kt
@@ -4,6 +4,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
+import com.twolskone.bakeroad.core.common.android.base.BaseComposable
 import com.twolskone.bakeroad.feature.report.detail.mvi.ReportDetailIntent
 
 @Composable
@@ -13,10 +14,12 @@ internal fun ReportDetailRoute(
 ) {
     val state by viewModel.state.collectAsStateWithLifecycle()
 
-    ReportDetailScreen(
-        state = state,
-        onBackClick = goBack,
-        onPreviousClick = { viewModel.intent(ReportDetailIntent.LoadPrevious) },
-        onNextClick = { viewModel.intent(ReportDetailIntent.LoadNext) }
-    )
+    BaseComposable(baseViewModel = viewModel) {
+        ReportDetailScreen(
+            state = state,
+            onBackClick = goBack,
+            onPreviousClick = { viewModel.intent(ReportDetailIntent.LoadPrevious) },
+            onNextClick = { viewModel.intent(ReportDetailIntent.LoadNext) }
+        )
+    }
 }
